4bbe1138dac109209bef5df8e8d9392f52bdb84d
John Jerome Angeles <jerome.lookingfour@gmail.com>
2021-09-02 11:19:19 +0800

    Fixes horizontal alignment

--- index.php
+++ index.php
@@ -94,7 +94,7 @@
 			</div>
 		</div> -->
 		<div id="contact-row" class="row text-center">
-			<div id="contact" class="col-md-10 offset-md-1 mt-3 pb-5 d-flex align-items-center">
+			<div id="contact" class="col-md-10 offset-md-1 mt-3 pb-5 d-flex justify-content-center">
 				<div>
 					<h1 class='mb-1'>Contact Information</h1>
 					<h3>Mobile Number: <b>+63-947-730-5745</b></h3>
